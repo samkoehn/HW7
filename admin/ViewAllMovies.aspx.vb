eb0131a8096d56aa5d8414b3a70fe02c49e91027
Sam Koehn <samantha-koehn@uiowa.edu>
2015-03-29 17:07:27 -0500

    admin functionality

---
+++ admin/ViewAllMovies.aspx.vb
@@ -0,0 +1,5 @@
+﻿
+Partial Class ViewAllMovies
+    Inherits System.Web.UI.Page
+
+End Class
---
+++ login/login.aspx.vb
@@ -0,0 +1,19 @@
+﻿
+Partial Class login_login
+    Inherits System.Web.UI.Page
+
+
+
+    Protected Sub Login1_LoggedIn(sender As Object, e As EventArgs) Handles Login1.LoggedIn
+
+        If Roles.IsUserInRole(Login1.UserName, "r_samkoehn_Admin") = True Then
+            Response.Redirect("~/admin/ViewAllMovies.aspx")
+
+        Else : Response.Redirect("~/Default.aspx")
+
+
+        End If
+
+
+    End Sub
+End Class
